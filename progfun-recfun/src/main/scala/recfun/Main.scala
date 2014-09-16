4281d8660baf21a09fe114868b5fd1662bf85156
dartafr <sclaudel@gmail.com>
2014-09-16 23:27:16 +0200

    Assignement ok

--- progfun-recfun/src/main/scala/recfun/Main.scala
+++ progfun-recfun/src/main/scala/recfun/Main.scala
@@ -1,5 +1,6 @@
 package recfun
 import common._
+import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
@@ -14,15 +15,45 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(colonne: Int, ligne: Int): Int = {
+    if (colonne == 0 || colonne == ligne) {
+      1
+    } else {
+      pascal(colonne - 1, ligne - 1) + pascal(colonne, ligne - 1)
+    }
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def checkBalance(chars: List[Char], stack: List[Char] = List()): Boolean = {
+      if (chars.isEmpty) {
+        stack.isEmpty;
+      } else if (chars.head == '(') {
+        checkBalance(chars.tail, chars.head :: stack)
+      } else if (chars.head == ')') {
+        !stack.isEmpty && stack.head != chars.head && checkBalance(chars.tail, stack.dropRight(1))
+      } else {
+        checkBalance(chars.tail, stack)
+      }
+    }
+    checkBalance(chars);
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) {
+      1
+    } else if (money < 0) {
+      0
+    } else if (coins.isEmpty) {
+      0
+    } else {
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
+    }
+  }
 }
